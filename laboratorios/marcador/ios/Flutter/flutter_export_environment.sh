315b9980c235fb1e742ba12de2b37acfa2ce2e6d
PJ-Lopez <lopezlopezpedro529@gmail.com>
2026-09-24 09:43:09 -0600

    marcador y correccion de archivos

---
+++ laboratorios/marcador/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\lopez\Documentos\8vo Semestre\progra Movil\pdm2026-202308038\laboratorios\marcador"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Users\lopez\Documentos\8vo Semestre\progra Movil\pdm2026-202308038\laboratorios\marcador\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
